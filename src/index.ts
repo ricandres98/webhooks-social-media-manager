413c857e7ed373a6497a1032c780027db4ceaa47
Ricardo <ricardo270298@gmail.com>
2024-09-11 13:22:45 -0400

    initial commit / verification endpoint

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (request, response) => {
+    response.send("Hola mundo");
+});
+
+app.listen(port, () => {
+    console.log("corriendo en el puerto " + port);
+});
+
+app.get("/webhooks", (req, res) => {
+    const query = req.query;
+    const hubMode = query["hub.mode"];
+    const hubChallenge = query["hub.challenge"];
+    const hubVerifyToken = query["hub.verify_token"];
+    console.log(query)
+
+    const verifyToken = process.env.VERIFY_TOKEN;
+
+    if(hubVerifyToken === verifyToken) {
+        res.status(200).json({
+            "hub.challenge": hubChallenge,
+        });
+    } else {
+        res.status(400).send({
+            error: "Invalid token"
+        });
+    }
+})
